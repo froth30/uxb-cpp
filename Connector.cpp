33a08b7dad27183612471fa704afdd26b759fb08
froth30 <theo.frohlich@gmail.com>
2017-04-07 02:48:25 -0400

    Implement 'Connector' class

--- Connector.cpp
+++ Connector.cpp
@@ -1,1 +1,18 @@
 #include "Connector.h"
+
+Connector::Connector(int index, Type type) {
+    this->index = index;
+    this->type = type;
+}
+
+int Connector::getIndex() {
+    return index;
+}
+
+Type Connector::getType() {
+    return type;
+}
+
+//Connector Connector::getPeer() {
+//    return peer;
+//}
