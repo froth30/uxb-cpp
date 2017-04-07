a31f55ec981c72a282caa97a3467b8348e2c3921
froth30 <theo.frohlich@gmail.com>
2017-04-07 01:44:08 -0400

    Add 'Connector' class

---
+++ Connector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by theof_000 on 4/7/2017.
+//
+
+#include "Connector.h"
---
+++ Connector.h
@@ -0,0 +1,14 @@
+//
+// Created by theof_000 on 4/7/2017.
+//
+
+#ifndef UXB_CONNECTOR_H
+#define UXB_CONNECTOR_H
+
+
+class Connector {
+
+};
+
+
+#endif //UXB_CONNECTOR_H
